616171726056758af07c2449ad1c07f7dcfdeb61
julia doroshina <yuspiri@gmail.com>
2023-02-21 10:34:07 +0100

    Добавь функцию isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,22 @@ const checkStringLength = (string, lengthLimit) => {
   return isLimitExceeded;
 };
 checkStringLength('проверяемая строка', 18);
+
+const isPalindrome = (string) => {
+  let isPalindromeResult = false;
+  string = string.toLowerCase().replaceAll(' ', '');
+  for (let i = 0; i <= string.length - 1; i++) {
+    let j = string.length - 1 - i;
+    if (string[i] === string[j]) {
+      isPalindromeResult = true;
+    } else {
+      return false;
 }
+  }
+  console.log(isPalindromeResult);
+  return isPalindromeResult;
+};
+isPalindrome('Лёша на полке клопа нашё ');
 
 checkStringLength ('проверяемая строка', 18);
 
