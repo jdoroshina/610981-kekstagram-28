204bde41d668ef484370702a542351e9c3d8630a
julia doroshina <yuspiri@gmail.com>
2023-03-23 11:51:49 +0100

    Добавь jsdoc comment для renderComments

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -2,9 +2,13 @@ const thumbnailTemplate = document
   .querySelector('#picture')
   .content
   .querySelector('.picture'); //Находим контент темплейта в разметке
-
 const container = document.querySelector('.pictures'); //находим место куда будем вставлять темплейт
 
+/**
+ * функция генерирования миниатюр
+ * @param {*} param0 объект описания
+ * @returns заполненный шаблон в виде html разметки
+ */
 const createThumbnail = ({ id, url, description, likes, comments }) => {
   const thumbnail = thumbnailTemplate.cloneNode(true); ///кладем клоны темплейтов в переменную
 
