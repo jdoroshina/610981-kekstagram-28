cf7be62f91ee50d841e8ed965b9492b8ba1c49f5
julia doroshina <yuspiri@gmail.com>
2023-03-23 12:09:00 +0100

    Настрой импорты и вызовы функций в main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { getPhotoCards } from './data.js';
 import { renderThumbnails } from './thumbnail.js';
 import './modal.js';
+import { createContainerListener } from './modal.js';
+const photoDescriptions = getPhotoCards();
 
-renderThumbnails(getPhotoCards());
+renderThumbnails(photoDescriptions);
+createContainerListener(photoDescriptions);
 
