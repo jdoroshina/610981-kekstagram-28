d06ae6a5d56d0064f4e9de14e3f8eb918c7bb305
julia doroshina <yuspiri@gmail.com>
2023-04-01 19:47:41 +0200

    Настрой валидацию формы через pristine

--- js/forms.js
+++ js/forms.js
@@ -1,5 +1,9 @@
 import { isEscapeKey } from './utils.js';
 
+const MAX_HASHTAG_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-zа-яë0-9]{1,19}$/i;
+const TAG_ERROR_TEXT = 'Введен неверный хэштег';
+
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const fileField = document.querySelector('#upload-file');
@@ -8,16 +12,23 @@ const form = document.querySelector('.img-upload__form');
 const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
 
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',//указываем элемент, на который добалвяем служебные классы
+  errorTextParent: 'img-upload__field-wrapper',//класс элемента, в который будет выводиться текст ошибки
+  errorTextClass: 'img-upload__field-wrapper__error'//класс для стилизация текста ошибки
+});
 
 const showModal = () => {
-  overlay.classlist.remove('hidden');
+  overlay.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 };
 
 const closeModal = () => {
+  form.reset();
+  pristine.reset();
   overlay.classList.add('hidden');
-  body.classList.add('modal-open');
+  body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
@@ -25,13 +36,13 @@ const isTextFieldFocused = () =>
   document.activeElement === hashtagField ||
   document.activeElement === commentField;
 
-//можно ли тут использовать не стрелочную функцию? пробовала решить как-то только стрелочными, но у меня не вышло
+// НЕ смогла придумать как переписать эту функцию в стрелочную?
 function onDocumentKeydown(evt) {
   if (isEscapeKey(evt) && !isTextFieldFocused()) {
     evt.preventDefault();
     closeModal();
   }
-};
+}
 
 const onFileInputChange = () => {
   showModal();
@@ -41,5 +52,34 @@ const onCancelButtonClick = () => {
   closeModal();
 };
 
+const hasValidCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+};
+
+pristine.addValidator(
+  hashtagField,//поле под валидацию
+  validateTags, // функция валидации
+  TAG_ERROR_TEXT //текст ошибки
+);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
 fileField.addEventListener('change', onFileInputChange);
 cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { getPhotoCards } from './data.js';
 import { renderThumbnails } from './thumbnail.js';
 import './modal.js';
 import { createContainerListener } from './modal.js';
+import './forms.js';
 const photoDescriptions = getPhotoCards();
 
 renderThumbnails(photoDescriptions);
