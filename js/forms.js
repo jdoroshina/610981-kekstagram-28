59504a5792d161b5b0ca86d334c4768de489de9a
julia doroshina <yuspiri@gmail.com>
2023-04-01 13:28:01 +0200

    Реализуй закрытие формы загрузки фото

--- js/forms.js
+++ js/forms.js
@@ -1,15 +1,45 @@
+import { isEscapeKey } from './utils.js';
+
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const fileField = document.querySelector('#upload-file');
+const cancelButton = document.querySelector('#upload-cancel');
+const form = document.querySelector('.img-upload__form');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
 
 
 const showModal = () => {
   overlay.classlist.remove('hidden');
   body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const closeModal = () => {
+  overlay.classList.add('hidden');
+  body.classList.add('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+//можно ли тут использовать не стрелочную функцию? пробовала решить как-то только стрелочными, но у меня не вышло
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt) && !isTextFieldFocused()) {
+    evt.preventDefault();
+    closeModal();
+  }
 };
 
 const onFileInputChange = () => {
   showModal();
 };
 
+const onCancelButtonClick = () => {
+  closeModal();
+};
+
 fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
