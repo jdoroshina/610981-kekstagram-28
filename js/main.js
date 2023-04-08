2d435eb0ceae856d3a9ff0cecd299cbec478fe10
julia doroshina <yuspiri@gmail.com>
2023-04-08 13:18:17 +0200

    Собери массив фильтров в effect.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import './modal.js';
 import { createContainerListener } from './modal.js';
 import './forms.js';
 import './scale.js';
+import './effect.js';
 const photoDescriptions = getPhotoCards();
 
 renderThumbnails(photoDescriptions);
