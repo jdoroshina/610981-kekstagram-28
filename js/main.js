f082c1aae3ea8f97e31713e2735e632908f55e43
julia doroshina <yuspiri@gmail.com>
2023-04-10 15:06:04 +0200

    Загрузи данные с сервера

--- js/main.js
+++ js/main.js
@@ -5,8 +5,13 @@ import { createContainerListener } from './modal.js';
 import './forms.js';
 import './scale.js';
 import './effect.js';
+
 const photoDescriptions = getPhotoCards();
 
-renderThumbnails(photoDescriptions);
-createContainerListener(photoDescriptions);
+fetch('https://28.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((thumbnails) => {
+    renderThumbnails(thumbnails);
+  });
 
+createContainerListener(photoDescriptions);
