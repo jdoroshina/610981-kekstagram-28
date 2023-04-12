aa6ddbcaef3edb3e31e561e533e6938026b05a41
julia doroshina <yuspiri@gmail.com>
2023-04-12 13:26:05 +0200

    Настрой сортировку фотографий

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getPhotoCards } from './data.js';
 import { renderThumbnails } from './thumbnail.js';
+import { getPicturesSorting } from './sorting.js';
 import { createContainerListener } from './modal.js';
 import { setUserFormSubmit, closeForm } from './forms.js';
 import { getData } from './api.js';
@@ -7,13 +8,13 @@ import './modal.js';
 import './forms.js';
 import './scale.js';
 import './effect.js';
-import './sorting.js';
 
 const photoDescriptions = getPhotoCards();
 
 getData()
   .then((thumbnails) => {
     renderThumbnails(thumbnails);
+    getPicturesSorting(thumbnails);
   });
 
 createContainerListener(photoDescriptions);
